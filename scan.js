073c2bed7bee304f208a762a293831e072cccdc8
narendraiiitl <lcs2019015@iiitl.ac.in>
2022-05-08 18:36:40 +0530

    updated

--- scan.js
+++ scan.js
@@ -1,16 +1,16 @@
 var scnbtn = document.getElementById("scn-btn");
 
-const BASE_URL = 'http://34e9-35-225-225-193.ngrok.io/test';
+const BASE_URL = 'http://c0e7-35-225-225-193.ngrok.io/test';
 
 const sendData = async (id, name, file) => {
     try {
         const formdata = { image: file, name: name, id: id };
         const data = { username: 'example' };
         const res = await fetch(BASE_URL, {
-            method: 'GET',
+            method: 'POST',
 
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded'
             },
         })
         return res;
