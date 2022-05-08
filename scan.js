eb0eaf80add1b0cf95efda3a719b147591ab5aea
narendraiiitl <lcs2019015@iiitl.ac.in>
2022-05-08 17:57:02 +0530

    updatd

--- scan.js
+++ scan.js
@@ -3,14 +3,20 @@ var scnbtn = document.getElementById("scn-btn");
 const BASE_URL = 'http://e1ca-35-225-225-193.ngrok.io/test';
 
 const sendData = async (id, name, file) => {
-    const formdata = { image: file, name: name, id: id };
-    const data = { username: 'example' };
-    const res = await fetch(BASE_URL, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
+    try {
+        const formdata = { image: file, name: name, id: id };
+        const data = { username: 'example' };
+        const res = await fetch(BASE_URL, {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
 
 };
 
@@ -18,6 +24,6 @@ scnbtn.addEventListener("click", () => {
     var id = document.getElementById("id").value;
     var name = document.getElementById("name").value;
     var file = document.getElementById("file").value;
-    const res = sendData(id, name, file);
+    const res = await sendData(id, name, file);
     console.log(res);
 })
